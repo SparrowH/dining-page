3536b834f3f5795c70d3d46e71bcc9dc7119a223
SparrowH <christophernyakotey@gmail.com>
2023-11-22 13:47:08 +0000

    final commit

--- src/components/Footer/FooterOverlay.jsx
+++ src/components/Footer/FooterOverlay.jsx
@@ -3,7 +3,11 @@ import './FooterOverlay.css'
 
 const FooterOverlay = () => {
   return (
-    <div>FooterOverlay</div>
+    <div className='app__footerOverlay' >
+      <div className='app__footerOverlay-black' ></div>
+      <div className='app__footerOverlay-img app__bg' ></div>
+
+    </div>
   )
 }
 
